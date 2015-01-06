4c1f4a0b63f48a7572f3aedadcc11e10985746d0
Hanneke van den Akker <hanneke@orikami.nl>
2015-01-06 16:06:00 +0100

    scraping

--- scraper_handelingen.py
+++ scraper_handelingen.py
@@ -1,38 +1,28 @@
 
-#from datetime import datetime
-
-#def generate_url(meet_numb, year, hand_numb): return
-#"https://zoek.officielebekendmakingen.nl/h-tk-(year)-(meet_numb)-(hand_numb).xml"
-
-
+#scraping tweede kamer handelingen. 
+#meldingen dat bestanden niet bestaan eruit filteren op de een of andere manier.
+jaartallen variabelen van maken
 #year=20132014 or 20142015
 #meet_numb = voor 20132014: 40-108
 #meet_numb = voor 20142015: 1-33
-#hand_numb = 1-15? of hoger? 
+#i = 1-40? of hoger? 
 
-#def run():
-#	output = open('', 'a')
-#	for meet_numb, year, hand_numb in 
-#	print 
-#	url= generate_url(meet_numb, year, hand_numb)
-#
 
-#import urllib
-#urllib.urlretrieve ("https://zoek.officielebekendmakingen.nl/h-tk-20142015-33-13.xml", "h-tk-20142015-33-13.xml")
+import urllib2
 
-#for meet_numb in range(40,109):  #to iterate between 10 to 20
- #  for i in range(1,6): #to iterate on the factors of the number
-  #     print 'h-tk-20132014-%d-%d' % (meet_numb,i)
+#onderstaande draait nu:
+for meet_numb in range(40,109):  
+	for i in range(1,40): 
+  		print 'h-tk-20132014-%d-%d' % (meet_numb,i)
+		resp = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d.xml' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
+		print resp.headers["content-type"]
 
-#output= open('h-tk-20142015-33-13.xml', 'wb')
 
-#import urllib2
-#response = urllib2.urlopen("https://zoek.officielebekendmakingen.nl/h-tk-20142015-33-13.xml", "h-tk-20142015-33-13.xml")
-#html = response.read()
-
-for meet_numb in range(40,109):  #to iterate between 10 to 20
-	for i in range(1,6): #to iterate on the factors of the number
-   		print 'h-tk-20132014-%d-%d' % (meet_numb,i)
+for meet_numb in range(1,33):  
+	for i in range(1,40): 
+   		print 'h-tk-20142015-%d-%d' % (meet_numb,i)
 		import urllib
-		urllib.urlretrieve ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
+		urllib.urlretrieve ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20142015-%d-%d' % (meet_numb,i), 'h-tk-20142015-%d-%d' % (meet_numb,i)+".xml")
+
+
 
