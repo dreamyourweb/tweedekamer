122f702d3831e6325dcef2b9c345729b7c6ffb78
Hanneke van den Akker <hanneke@orikami.nl>
2015-01-06 17:05:03 +0100

    scraping #2

--- scraper_handelingen.py
+++ scraper_handelingen.py
@@ -1,28 +1,30 @@
 
-#scraping tweede kamer handelingen. 
-#meldingen dat bestanden niet bestaan eruit filteren op de een of andere manier.
-jaartallen variabelen van maken
-#year=20132014 or 20142015
-#meet_numb = voor 20132014: 40-108
-#meet_numb = voor 20142015: 1-33
-#i = 1-40? of hoger? 
+"""scraping tweede kamer handelingen. 
+Hoe meldingen dat bestanden niet bestaan ("De pagina die u zocht kon niet worden gevonden")
+eruit filteren?
+meet_numb = voor 20132014: 40-108
+meet_numb = voor 20142015: 1-33
+i = 1-40? of hoger? 
+20132014-variabele maken 
+"""
 
 
 import urllib2
 
-#onderstaande draait nu:
+
 for meet_numb in range(40,109):  
 	for i in range(1,40): 
   		print 'h-tk-20132014-%d-%d' % (meet_numb,i)
-		resp = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d.xml' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
-		print resp.headers["content-type"]
-
+		response = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d.xml' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
+		output = open('h-tk-20132014-%d-%d' % (meet_numb,i)+".xml", 'wb')
+		output.write(response.read())
+		output.close()
 
 for meet_numb in range(1,33):  
 	for i in range(1,40): 
-   		print 'h-tk-20142015-%d-%d' % (meet_numb,i)
-		import urllib
-		urllib.urlretrieve ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20142015-%d-%d' % (meet_numb,i), 'h-tk-20142015-%d-%d' % (meet_numb,i)+".xml")
-
-
+  		print 'h-tk-20142015-%d-%d' % (meet_numb,i)
+		response = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20142015-%d-%d' % (meet_numb,i), 'h-tk-20142015-%d-%d' % (meet_numb,i)+".xml")
+		output = open('h-tk-20132014-%d-%d' % (meet_numb,i)+".xml", 'wb')
+		output.write(response.read())
+		output.close()
 
