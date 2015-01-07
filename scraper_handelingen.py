5ec4612564d164ffbcd7d259991c5d9af79a5dfa
Hanneke van den Akker <hanneke@orikami.nl>
2015-01-07 13:16:43 +0100

    scraping with exclusion of 404

--- scraper_handelingen.py
+++ scraper_handelingen.py
@@ -1,31 +1,37 @@
 
 """scraping tweede kamer handelingen. 
-Hoe meldingen dat bestanden niet bestaan ("De pagina die u zocht kon niet worden gevonden")
-eruit filteren? urllib2.httperror? 
 meet_numb = voor 20132014: 40-108
 meet_numb = voor 20142015: 1-33
 i = 1-40? of hoger? 
 20132014-variabele maken 
-"""
 
+"""
 
 import urllib2
 
 
 for meet_numb in range(40,109):  
-	for i in range(1,40): 
-  		print 'h-tk-20132014-%d-%d' % (meet_numb,i)
-		response = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d.xml' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
-		output = open('h-tk-20132014-%d-%d' % (meet_numb,i)+".xml", 'wb')
-		output.write(response.read())
-		output.close()
+    for i in range(1,40): 
+        print 'h-tk-20132014-%d-%d' % (meet_numb,i)
+        response = urllib2.urlopen("https://zoek.officielebekendmakingen.nl/" +'h-tk-20132014-%d-%d.xml' % (meet_numb,i), 'h-tk-20132014-%d-%d' % (meet_numb,i)+".xml")
+        if response.info().getheader('Content-Type') == "text/html":
+            break
+        else:
+            output = open('data/h-tk-20132014-%d-%d' % (meet_numb,i)+".xml", 'wb')
+            output.write(response.read())
+            output.close()
+
+
+
 
 for meet_numb in range(1,34):  
-	for i in range(1,40): 
-  		print 'h-tk-20142015-%d-%d' % (meet_numb,i)
-		response = urllib2.urlopen ("https://zoek.officielebekendmakingen.nl/" +'h-tk-20142015-%d-%d' % (meet_numb,i), 'h-tk-20142015-%d-%d' % (meet_numb,i)+".xml")
-		Output = open('h-tk-20142015-%d-%d' % (meet_numb,i)+".xml", 'wb')
-		output.write(response.read())
-		output.close()
-
+    for i in range(1,40): 
+        print 'h-tk-20142015-%d-%d' % (meet_numb,i)
+        response = urllib2.urlopen("https://zoek.officielebekendmakingen.nl/" +'h-tk-20142015-%d-%d.xml' % (meet_numb,i), 'h-tk-20142015-%d-%d' % (meet_numb,i)+".xml")
+        if response.info().getheader('Content-Type') == "text/html":
+            break
+        else:
+            output = open('data/h-tk-20142015-%d-%d' % (meet_numb,i)+".xml", 'wb')
+            output.write(response.read())
+            output.close()
 
